68aa64b61144554b47d25ab8aed04408b51e54df
Soe Sandar Kyi GICM <soesandarkyi@gicjp.com>
2025-08-25 16:15:11 +0630

    controller setting

---
+++ GroceryShopSystem/Controllers/Admin/OrdersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryShopSystem.Controllers.Admin
+{
+	public class OrdersController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}

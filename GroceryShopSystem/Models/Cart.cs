84573edae3ce2d0d2fca3590d87f92f11249e429
aungmyatthu <aungmyatthu@gicjp.com>
2025-08-25 08:48:56 +0630

    add model class

---
+++ GroceryShopSystem/Models/Cart.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GroceryShopSystem.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }   // Django auto-generated id
+
+        // One-to-One with Customer (User)
+        [Required]
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        // TODO
+        // After create user, User add yan        
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        
+    }
+}
---
+++ GroceryShopSystem/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GroceryShopSystem.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }  
+
+        // Foreign key for Category
+        [Required]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be >= 0")]
+        public decimal Price { get; set; }
+        
+        public string ImageUrl { get; set; }    // pyan phyote yan    
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;        
+
+        [Required]
+        public bool IsActive { get; set; } = true;
+    }
+}

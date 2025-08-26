613c4886cdfccd0fd6e649b50f073ce4cae9e8ad
Soe Sandar Kyi GICM <soesandarkyi@gicjp.com>
2025-08-26 09:02:28 +0630

    add acc controller

---
+++ GroceryShopSystem/Controllers/Account/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using GroceryShopSystem.Models;
+using GroceryShopSystem.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GroceryShopSystem.Controllers.Account
+{
+	public class AccountController : Controller
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+
+		public AccountController(UserManager<ApplicationUser> userManager,
+			SignInManager<ApplicationUser> signInManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		[HttpGet]
+		public IActionResult Logint() => View();
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+		{
+			if (!ModelState.IsValid) return View(loginViewModel);
+
+			var user = await _userManager.FindByEmailAsync(loginViewModel.Email);
+
+			if (user != null)
+			{
+				//User is found, check password
+				var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+				if (passwordCheck)
+				{
+					//Password correct, sign in
+					var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+					if (result.Succeeded)
+					{
+						return RedirectToAction("Index", "Dashboard");
+					}
+				}
+				//Password is incorrect
+				TempData["Error"] = "Wrong credentials. Please try again";
+				return View(loginViewModel);
+			}
+			//User not found
+			TempData["Error"] = "Wrong credentials. Please try again";
+			return View(loginViewModel);
+		}
+	}
+}

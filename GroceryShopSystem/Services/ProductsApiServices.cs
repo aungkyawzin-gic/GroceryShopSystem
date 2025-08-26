2d93e84a7e79aa655d958dad604a8b998799cc9a
aungmyatthu <aungmyatthu@gicjp.com>
2025-08-26 13:48:00 +0630

    add controller and rest controller

---
+++ GroceryShopSystem/Services/ProductsApiServices.cs
@@ -0,0 +1,25 @@
+﻿using GroceryShopSystem.Models;
+
+namespace GroceryShopSystem.Services
+{
+    public class ProductsApiServices
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductsApiServices(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:7074/"); // Your base URL
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<Product>>("api/productsApi");
+        }
+
+        public async Task<Product?> GetProductAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Product>($"api/productsApi/{id}");
+        }
+    }
+}

07bacf9fd86184af55e4f52622e1b657cd97dc00
Kay Thi Nwe <kaythinwe@gicjp.com>
2025-08-27 16:18:16 +0630

    Add: User Api

---
+++ GroceryShopSystem/ViewModels/UserCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GroceryShopSystem.ViewModels
+{
+
+    public class UserCreateViewModel
+    {
+        public string FullName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+}

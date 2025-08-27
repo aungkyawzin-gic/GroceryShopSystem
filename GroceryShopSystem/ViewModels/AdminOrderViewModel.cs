fa0080413d532f1127571a4f78466902f44b382f
nandarwutyi <nandarwutyi@gicjp.com>
2025-08-28 06:16:56 +0630

    add admin orders api

---
+++ GroceryShopSystem/ViewModels/AdminOrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace GroceryShopSystem.ViewModels
+{
+    public class AdminOrderViewModel
+    {
+        public int OrderId { get; set; }
+        public string OrderNo { get; set; }
+        public string UserName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal ShippingPrice { get; set; }
+        public decimal Tax { get; set; }
+        public decimal GrandTotal { get; set; }
+        public string Status { get; set; }
+        public string Remark { get; set; }
+        public List<OrderItemViewModel> Items { get; set; }
+    }
+}

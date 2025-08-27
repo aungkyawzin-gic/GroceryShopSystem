1b77a42ad006352ddfc681b6a492ff41585f5cd1
phombmyintmaunggicjp <phombmyintmaung@gicjp.com>
2025-08-27 14:14:46 +0630

    cart cant connet api

---
+++ GroceryShopSystem/ViewModels/CartItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GroceryShopSystem.ViewModels
+{
+    public class CartItemViewModel
+    {
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
